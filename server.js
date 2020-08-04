9efcfb5d96222383204612d0f020f81851594a72
Pulkit-Bhutani <pulkitbhutani15@gmail.com>
2020-08-04 22:43:37 +0530

    changes for subfolder location

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/*', function(req, res) {
 });
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/Assignment'));
 // Start the app by listening on the default
 // Heroku port
 const forceSSL = function () {
