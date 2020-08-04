69c94c72a8b8310f0faed146cb3d0987a30f04e5
Pulkit-Bhutani <pulkitbhutani15@gmail.com>
2020-08-04 22:36:58 +0530

    chsnges for directory

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/Assignment/index.html'));
 });
 // Run the app by serving the static files
 // in the dist directory
